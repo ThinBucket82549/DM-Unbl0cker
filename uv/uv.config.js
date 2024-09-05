58d3d197cd872e2f7cdcc91c8392cf1a2e8f66eb
ThinBucket82549 <73562905+ThinBucket82549@users.noreply.github.com>
2024-09-05 11:23:59 +1000

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://dm-unbl0cker-bare-server-production-37d4.up.railway.app',
+    bare: 'https://collegeapp.me/v/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
